c918d2b712232e631787ba50d3e4824e493d49d2
Travis Newman <travis.c.newman@gmail.com>
2016-06-07 17:50:32 -0400

    completed main page styling

--- js/galax-running-club.js
+++ js/galax-running-club.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+
+jQuery(document).ready(function($) {
+	
+	// Lightcase activation
+	$('a[data-rel^=lightcase]').lightcase({
+			maxWidth: 1200,
+			maxHeight: 800,
+			showCaption: false
+	});
+
+
+	// Set active class automatically
+	var url = window.location;
+	// Will only work if string in href matches with location
+	$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+	// Will also work for relative and absolute hrefs
+	$('ul.nav a').filter(function() {
+	    return this.href == url;
+	}).parent().addClass('active');
+
+});
